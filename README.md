9f290c8df1063d66d85058f3714486839afc26db
Daiana de Paula <daianaadepaula1@gmail.com>
2024-05-02 11:05:43 -0300

    feat: anatomia de classes - declarando variáveis e métodos

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Repositório para armazenar todo o conteúdo do curso de Java Básico.
 ## Anatomia de Classes
 
 [x] Padrão de Nomeclatura  
-[ ] Declarando Variáveis e Métodos  
+[x] Declarando Variáveis e Métodos  
 [ ] Indentação  
 [ ] Organizando Arquivos  
 [ ] Java Beans
---
+++ java-anatomia-classes/src/VariavelEMetodos.java
@@ -0,0 +1,25 @@
+public class VariavelEMetodos {
+	
+	public static void main(String[] args)  { 				
+
+		String meuNome = "Daiana";
+		int anoFabricacao = 2023;
+		double altura = 1.62;
+		boolean verdadeira;
+		boolean verdadeira2 = false;
+		Dog spike; //observe que aqui a variável spike não tem valor, é normal
+
+		anoFabricacao = 2018; 
+
+		String primeiroNome = "Daiana";
+		String segundoNome = "Araujo"; 
+
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto);
+
+	}
+
+	public static String nomeCompleto (String primeiroNome, String segundoNome){
+		return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+}
